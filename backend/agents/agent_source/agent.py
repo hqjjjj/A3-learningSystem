635b12a101637ed69d72dbb83d8754b412f27fcc
hequanjing <2123396088@qq.com>
2026-05-09 10:04:13 +0800

    测试分支创建

--- backend/agents/agent_source/agent.py
+++ backend/agents/agent_source/agent.py
@@ -2,7 +2,7 @@
 from prompts import build_prompt
 from data.knowledge.KnowledgeBaseManager import KnowledgeBaseManager
 kb=KnowledgeBaseManager()
-
+# 111
 class agentInput:
     topic_id:str
     module:str
