9ae1c7b2d4f08faecf1de57cd4eb93f9f7ccbec0
hequanjing <2123396088@qq.com>
2026-07-13 14:02:15 +0800

    llm模型更换

--- backend/agents/agent_source/lmm.py
+++ backend/agents/agent_source/lmm.py
@@ -3,13 +3,13 @@ from openai import OpenAI
 class SparkLLM:
     def __init__(self):
         self.client = OpenAI(
-            api_key="RsgGrlvXfBMNuLCrewHn:LocQjXtrcchLshPHORMf",  
-            base_url="https://spark-api-open.xf-yun.com/agent/v1/",
+            api_key="sk-ws-H.EDDMREL.BYsw.MEYCIQDKftr9UEUcLCamIm3BIjl5jNFDyG8c1pfCNf3bWfgkpwIhAI3U6AFl3ZGu14diTmHfXsPQAuNTImUr98h-Wx7dJFQ8",  # 换成你实际用的密钥
+            base_url="https://ws-hr5a678q05zwljxu.cn-beijing.maas.aliyuncs.com/compatible-mode/v1", 
         )
 
     def generate(self, system_prompt: str, user_prompt: str) -> str:
         response = self.client.chat.completions.create(
-            model="spark-x",
+            model="qwen-plus",  
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt}
